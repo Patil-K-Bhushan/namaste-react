f69ad57c4e5c13b9f0bdf8711c56ca948eed88a5
Bhushan Patil <bpatil.00001@gmail.com>
2025-06-25 13:55:49 +0530

    episode-03 part-1

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1",{id:"heading"},"Namaste React🚀");
+
+console.log(heading);
+
+const jsxHeading =(<h1 id="heading">Namaste React using JSX🚀</h1>);
+
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
